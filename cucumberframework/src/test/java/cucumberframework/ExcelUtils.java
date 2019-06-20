700545f58d11bce43fa3261fa2bea50462564f56
PavanK <PavanK@PavanK-PC>
2019-06-20 23:55:53 +0530

    Excel util

--- cucumberframework/src/test/java/cucumberframework/ExcelUtils.java
+++ cucumberframework/src/test/java/cucumberframework/ExcelUtils.java
@@ -1,9 +1,22 @@
 package cucumberframework;
 
+import java.io.IOException;
+
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+
 public class ExcelUtils {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		
+		try {
+			XSSFWorkbook wb=new XSSFWorkbook("F:/nari/eclipse-jee-oxygen-3a-win32-x86_64/abcd.xlsx");
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
 
 	}
 
