3f7884a1581a1527201923153c408810cc73cca6
PavanK <PavanK@PavanK-PC>
2019-06-21 02:19:15 +0530

    updated excel reader

--- cucumberframework/src/test/java/cucumberframework/ExcelUtils.java
+++ cucumberframework/src/test/java/cucumberframework/ExcelUtils.java
@@ -2,6 +2,8 @@ package cucumberframework;
 
 import java.io.IOException;
 
+import org.apache.poi.xssf.usermodel.XSSFCell;
+import org.apache.poi.xssf.usermodel.XSSFRow;
 import org.apache.poi.xssf.usermodel.XSSFSheet;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 
@@ -12,6 +14,25 @@ public class ExcelUtils {
 		
 		try {
 			XSSFWorkbook wb=new XSSFWorkbook("F:/nari/eclipse-jee-oxygen-3a-win32-x86_64/abcd.xlsx");
+		
+		XSSFSheet sh= wb.getSheet("sheet1");
+		XSSFRow row=sh.getRow(0);
+		int col=row.getLastCellNum();
+		int rownum=sh.getLastRowNum()+1;
+		//XSSFCell celldata=sh.getRow(0).getCell(1);
+	for(int i=0;i<rownum;i++)
+	{
+		for(int j=0;j<col;j++)
+		{
+			XSSFCell celldata=sh.getRow(i).getCell(j);
+			System.out.println(celldata);
+			
+		}
+		
+	}
+			
+			
+		
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
